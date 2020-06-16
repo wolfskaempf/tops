86604a30a1c29fae09bcc7317bced72224b854ef
wolfskaempf <tom@wolfskaempf.de>
2020-06-16 18:35:28 +0200

    add button styling, fix missing }

--- app/static/js/clipboard.js
+++ app/static/js/clipboard.js
@@ -1,10 +1,11 @@
 function copy() {
-    var markdownDiv = document.getElementById("markdown");
+    let markdownDiv = document.getElementById("markdown");
 
-    var text = markdownDiv.innerText;
+    let text = markdownDiv.innerText;
     navigator.clipboard.writeText(text).then(function () {
         console.log('Async: Copying to clipboard was successful!');
     }, function (err) {
         console.error('Async: Could not copy text: ', err);
     });
+}
 
