ec43a3adbf4e176334edd48954adce67792e5363
Tom Wolfskämpf <tom@wolfskaempf.de>
2022-02-17 22:04:06 +0100

    fix date being rejected due to new date picker format

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Optional
 class CreateTopForm(FlaskForm):
     titel = StringField('Titel', validators=[DataRequired()])
     eingereicht_von = StringField('Eingereicht von', validators=[DataRequired()])
-    frist = DateField('Frist', format=("%d.%m.%Y"), validators=[Optional()])
+    frist = DateField('Frist', validators=[Optional()])
     beschreibung = TextAreaField('Beschreibung')
     abstimmungsfragen = TextAreaField('Abstimungsfragen')
     submit = SubmitField('Einreichen')
